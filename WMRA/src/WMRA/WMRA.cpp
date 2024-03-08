349498e518604afa6ee9b669fde1d11e4b68c413
WD <944740137@qq.com>
2024-03-08 14:46:28 +0800

    3.8_14.46

--- WMRA/src/WMRA/WMRA.cpp
+++ WMRA/src/WMRA/WMRA.cpp
@@ -10,7 +10,7 @@
 #include "math.h"
 
 void pub(const ros::Publisher &odomPublisher, ros::Publisher &pathPublisher, tf::TransformBroadcaster &odomBroadcaster,
-         double &v, double &w, double &x, double &y, double &theta);
+         nav_msgs::Path &path, double &v, double &w, double &x, double &y, double &theta);
 
 int main(int argc, char *argv[])
 {
@@ -20,9 +20,14 @@ int main(int argc, char *argv[])
     WheelChair wheelChair;
     // Manipulator manipulator(false); // 阻塞等待机械臂连接成功
 
+    // pub
     ros::Publisher odomPublisher = n.advertise<nav_msgs::Odometry>("/WMRA/odom", 10);
-    ros::Publisher pathPublisher = n.advertise<nav_msgs::Path>("trajectory", 1, true);
+    ros::Publisher pathPublisher = n.advertise<nav_msgs::Path>("/WMRA/trajectory", 1, true);
     tf::TransformBroadcaster odomBroadcaster;
+    nav_msgs::Path path;
+    ros::Time currentTime = ros::Time::now();
+    path.header.stamp = currentTime;
+    path.header.frame_id = "odom";
 
     //
     double Vd = 0;
@@ -70,10 +75,7 @@ int main(int argc, char *argv[])
         Wd = atan2(vyd, vxd);
         Vd = 0.05;
         Wd = 0;
-        // if (time >= 2)
-        // {
-        // Vd = 0.00;
-        // }
+
         std::cout << "x:" << x << "  y:" << y << std::endl;
 
         // run
@@ -81,7 +83,8 @@ int main(int argc, char *argv[])
         // manipulator.run(q_d, dq_d, tau_feedforward);
 
         // pub
-        pub(odomPublisher, pathPublisher, odomBroadcaster, V, W, x, y, theta);
+        pub(odomPublisher, pathPublisher, odomBroadcaster, path, V, W, x, y, theta);
+
         //
         ros::spinOnce();
         r.sleep();
@@ -91,7 +94,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 void pub(const ros::Publisher &odomPublisher, ros::Publisher &pathPublisher, tf::TransformBroadcaster &odomBroadcaster,
-         double &v, double &w, double &x, double &y, double &theta)
+         nav_msgs::Path &path, double &v, double &w, double &x, double &y, double &theta)
 {
     ros::Time currentTime = ros::Time::now();
     // odom_trans
@@ -118,8 +121,8 @@ void pub(const ros::Publisher &odomPublisher, ros::Publisher &pathPublisher, tf:
     odom.twist.twist.linear.y = v * sin(theta);
     odom.twist.twist.angular.z = w;
     odomPublisher.publish(odom);
+
     // path
-    nav_msgs::Path path;
     geometry_msgs::PoseStamped this_pose_stamped;
     this_pose_stamped.header.stamp = currentTime;
     this_pose_stamped.pose.position.x = x;
@@ -135,3 +138,74 @@ void pub(const ros::Publisher &odomPublisher, ros::Publisher &pathPublisher, tf:
     path.poses.push_back(this_pose_stamped);
     pathPublisher.publish(path);
 }
+
+// #include <ros/ros.h>
+// #include <ros/console.h>
+// #include <nav_msgs/Path.h>
+// #include <std_msgs/String.h>
+// #include <geometry_msgs/Quaternion.h>
+// #include <geometry_msgs/PoseStamped.h>
+// #include <tf/transform_broadcaster.h>
+// #include <tf/tf.h>
+
+// main (int argc, char **argv)
+// {
+//     ros::init (argc, argv, "showpath");
+
+//     ros::NodeHandle ph;
+//     ros::Publisher path_pub = ph.advertise<nav_msgs::Path>("trajectory",1, true);
+
+//     ros::Time current_time, last_time;
+//     current_time = ros::Time::now();
+//     last_time = ros::Time::now();
+
+//     nav_msgs::Path path;
+//     //nav_msgs::Path path;
+//     path.header.stamp=current_time;
+//     path.header.frame_id="odom";
+
+//     double x = 0.0;
+//     double y = 0.0;
+//     double th = 0.0;
+//     double vx = 0.1;
+//     double vy = -0.1;
+//     double vth = 0.1;
+
+//     ros::Rate loop_rate(1);
+//     while (ros::ok())
+//     {
+
+//         current_time = ros::Time::now();
+//         //compute odometry in a typical way given the velocities of the robot
+//         double dt = 0.1;
+//         double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+//         double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+//         double delta_th = vth * dt;
+
+//         x += delta_x;
+//         y += delta_y;
+//         th += delta_th;
+
+//         geometry_msgs::PoseStamped this_pose_stamped;
+//         this_pose_stamped.pose.position.x = x;
+//         this_pose_stamped.pose.position.y = y;
+
+//         geometry_msgs::Quaternion goal_quat = tf::createQuaternionMsgFromYaw(th);
+//         this_pose_stamped.pose.orientation.x = goal_quat.x;
+//         this_pose_stamped.pose.orientation.y = goal_quat.y;
+//         this_pose_stamped.pose.orientation.z = goal_quat.z;
+//         this_pose_stamped.pose.orientation.w = goal_quat.w;
+
+//         this_pose_stamped.header.stamp=current_time;
+//         this_pose_stamped.header.frame_id="odom";
+//         path.poses.push_back(this_pose_stamped);
+
+//         path_pub.publish(path);
+//         ros::spinOnce();               // check for incoming messages
+
+//         last_time = current_time;
+//         loop_rate.sleep();
+//     }
+
+//     return 0;
+// }
